65190b3a1ddeeb9867028517375d806af9a5b643
Kang <blues0910@hotmail.com>
2021-06-15 13:14:33 +0900

    Delete NRMSE.m

--- NRMSE.m
+++
@@ -1,9 +0,0 @@
-function v = NRMSE(y,yhat)
-%NRMSE 此处显示有关此函数的摘要
-%   此处显示详细说明
-M=length(y);
-tmp1=sum((y - yhat).^2);   % Mean Squared Error
-tmp2=std(y)^2;
-v= sqrt(tmp1/(M*tmp2));  % Normalised Root Mean Square Error
-end
-
