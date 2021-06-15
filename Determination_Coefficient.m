81a1f90fce130d9467840f1e4906e59b2ee7596d
Kang <blues0910@hotmail.com>
2021-06-15 13:13:53 +0900

    Delete Determination_Coefficient.m

--- Determination_Coefficient.m
+++
@@ -1,9 +0,0 @@
-function [d] = Determination_Coefficient(u,y,y_hat)
-%DETERMINATION_COEFFICIENT 此处显示有关此函数的摘要
-%   此处显示详细说明
-c=cov(y,y_hat);
-std1=std(u);
-std2=std(y_hat);
-d=c(1,2)^2/(std1*std2)^2;
-end
-
